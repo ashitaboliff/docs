7f054bedc19bac97edd49e51c8b736fae1ebb515
watabegg <watabegg@gmail.com>
2026-01-20 04:21:06 +0900

    feat: ドキュメントの更新と新規ファイルの追加
    - `docs/intro.md`にWeb開発基礎知識へのリンクを追加
    - `docs/setup.md`を削除し、`docs/setup/_category_.json`を新規作成
    - `docs/setup/wsl.mdx`を新規作成し、WSLのインストール手順を追加
    - `docs/tutorial-basics`内の複数のファイルを削除
    - `docs/tutorial-extras`内の複数のファイルを削除
    - `docs/web-development/_category_.json`の位置を変更
    - `docs/web-development/next.mdx`を新規作成し、Next.jsについての説明を追加
    - `docs/web-development/react.mdx`にReactの詳細な説明を追加
    - `docs/web-development/web-app.mdx`にWebアプリの仕組みとサンプルコードを追加
    - `docusaurus.config.ts`の編集URLを更新
    - `src/components/Figure/styles.module.css`に画像スタイルを追加
    - `static/docs/web-dev/web-app/sample.html`を新規作成し、シンプルなWebアプリのサンプルを追加

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,8 +38,6 @@ const config: Config = {
 			{
 				docs: {
 					sidebarPath: './sidebars.ts',
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
 					editUrl: 'https://github.com/ashitaboliff/docs/edit/main/',
 				},
 				blog: {
@@ -48,10 +46,7 @@ const config: Config = {
 						type: ['rss', 'atom'],
 						xslt: true,
 					},
-					// Please change this to your repo.
-					// Remove this to remove the "edit this page" links.
 					editUrl: 'https://github.com/ashitaboliff/docs/edit/main/',
-					// Useful options to enforce blogging best practices
 					onInlineTags: 'warn',
 					onInlineAuthors: 'warn',
 					onUntruncatedBlogPosts: 'warn',
