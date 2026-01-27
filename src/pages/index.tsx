cfc69c542c4b588c97c83980936a2b22b93fa02a
watabegg <watabegg@gmail.com>
2026-01-27 22:48:55 +0900

    fix: 環境構築へのリンクを修正
    リンク先を"/docs/setup"から"/docs/category/setup"に変更しました。

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ function HomepageHeader() {
 				<div className={styles.buttons}>
 					<Link
 						className="button button--secondary button--lg"
-						to="/docs/setup"
+						to="/docs/category/setup"
 					>
 						環境構築を始める 🚀
 					</Link>
